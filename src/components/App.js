c01da4403d4213acc3e423173aa40f1f5f21c8a1
Muhammad Abuzar <72570410+abzuar9658@users.noreply.github.com>
2021-09-16 10:10:12 +0500

    frontend with Auth

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Login from "./Auth/Login";
+import Register from "./Auth/Register";
+import Home from "./Home/Home";
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/flight/:id">{/* <About /> */}</Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
